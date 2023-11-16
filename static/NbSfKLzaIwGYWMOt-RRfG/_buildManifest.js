dbb501df8bc0aa8d1e3a82301abcb2fedc832037
PauloAbrahao <ab.cod@outlook.com>
2023-11-16 11:35:55 -0300

    Updates

---
+++ static/NbSfKLzaIwGYWMOt-RRfG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/b70a55e07e153a86.css","static/chunks/pages/index-245745386b4aa598.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
